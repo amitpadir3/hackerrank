aace01764f71be2b799a99c34e8533cf9531ec14
Amit Padir <51291344+amitpadir3@users.noreply.github.com>
2021-06-14 10:53:32 +0530

    Create StringReverseRecursion.java

---
+++ HackerRank/src/com/hackerRank/java/string/StringReverseRecursion.java
@@ -0,0 +1,18 @@
+package string;
+
+public class StringReverseRecursion {
+
+	public static void main(String ar[]) {
+		String s = reverse("abcd");
+		System.out.println(s);
+	}
+
+	private static String reverse(String s) {
+		if (s.length() == 1) {
+			return s;
+		}
+
+		return reverse(s.substring(1)) + s.charAt(0);
+	}
+
+}
